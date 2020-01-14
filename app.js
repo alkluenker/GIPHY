41c682a2d3ee34a8e109d41383157f7ddc4e98cc
Angela Kluenker <alkluenker@hotmail.com>
2020-01-14 16:39:22 -0600

    gifs displaying

--- app.js
+++ app.js
@@ -19,4 +19,42 @@ function makeButtons() {
     }
 }
 
+function showMovieTitle() {
+}
+
+$("#moviebutton").on("click", ".movies", function() {
+    
+    var movieName = $(this).attr("data-name");
+    console.log(movieName);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=fzUhFGJr5hhKrHauSJqFT8FbvAvc15tN&q=" + movieName + "&limit=25&offset=0&rating=G&lang=en"
+    
+    $.ajax({
+    
+        url: queryURL,
+        method: "GET"
+    
+    }).then(function(response) {
+
+        $("#viewgifs").empty();
+
+        for (a = 0; a < 10; a++) {
+
+            var imgURL = response.data[a].images.original.url;
+
+            var image = $("<img>").attr("src", imgURL)
+        
+            $("#viewgifs").append(image);
+
+        }
+    
+    });
+
+}) 
+
+    
+
+
+
+
 makeButtons();
